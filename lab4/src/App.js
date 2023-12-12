d023e2517e5aceead8b3614e39f525f3ca490117
Darneur <20408795+Darneur@users.noreply.github.com>
2023-12-12 14:09:14 -0500

    first

---
+++ lab4/src/App.js
@@ -0,0 +1,38 @@
+import {Button, TextField, Grid, Box} from '@mui/material';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  function Popup() {
+    alert('Signup successful!')
+  }
+
+  return (
+    <div className="App">
+    <Grid container spacing={2}>
+      <Grid item xs={12} justifyContent="center" alignItems="center" backgroundColor="lightBlue">
+      <h1 style={{color:'white'}}>Human Computer Interaction Newsletter</h1>
+      </Grid>
+      <Grid item xs={8} display="flex" justifyContent="center" alignItems="center">
+      <p>Signup for the human computer interaction newsletter!</p>
+      <TextField id="outlined-basic" type="email" label="email@wpi.edu" variant="outlined" style={{marginRight: '20px'}}/>
+      <Button variant='contained' onClick={Popup}>Sign Up</Button>
+      </Grid>
+      <Grid item xs={4} display="flex" justifyContent="center" alignItems="center">
+              <Box
+        component="img"
+        sx={{
+          height: '100%',
+          width: '100%',
+        }}
+        alt="A react logo"
+        src={logo}
+      />  
+      </Grid>
+    </Grid>
+    </div>
+  );
+}
+
+export default App;
